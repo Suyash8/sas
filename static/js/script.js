3b4e847c892686540694304fd895cf630ed0accc
Suyash8 <77190293+Suyash8@users.noreply.github.com>
2022-01-11 10:28:39 +0000

    Added footer

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
+const body = document.querySelector("body");
 
 btn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
