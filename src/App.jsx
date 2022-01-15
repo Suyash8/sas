57d3633f369fd64f5efe790735e3156759a02cc1
Suyash8 <77190293+Suyash8@users.noreply.github.com>
2022-01-15 09:09:57 +0000

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
- import React from 'react';
+import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar"
+
 
 function App() {
-  return (
-    <main>
-      React⚛️ + Vite⚡ + Replit🌀
-    </main>
-  );
+    return (
+        <main className="flex justify-center items-center">
+            <Navbar />
+        </main>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar(props) {
+    return (
+        <header>
+            <nav>
+                This is the Navbar
+            </nav>
+        </header>
+    )
+}
